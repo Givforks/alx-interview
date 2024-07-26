34dcfe7f71738bb3a90c829416c81aba206857bd
Givens Emmah Abraham <140185547+Givforks@users.noreply.github.com>
2024-07-26 11:55:49 +0100

    Update 0-validate_utf8.py
    I understood that I had identification issue.

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,25 +1,39 @@
 #!/usr/bin/python3
-"""consider if given data set reps valid UTF-8 encoding.
+"""
+Checks if a given data set
+represents a valid UTF-8 encoding.
 """
 
 
 def validUTF8(data):
-"""consider if given data set reps valid UTF-8 encoding.
-"""
-    s = 0
-    t = 1 << 6
-    for n in data:
+    """Checks if a given dataset represents utf8."""
+    n_bytes, mask1, mask2 = 0, 1 << 7, 1 << 6
+
+    for num in data:
         mask = 1 << 7
-        if s == 0:
-            while n & mask:
-                s += 1
+        if n_bytes == 0:
+            while mask & num:
+                n_bytes += 1
                 mask = mask >> 1
-            if s > 4 or s == 1:
-                return False
-            if s == 0:
+            if n_bytes == 0:
                 continue
-        else:
-            if not (n & mask and not (n & t)):
+            if n_bytes == 1 or n_bytes > 4:
                 return False
-        s -= 1
-    return s == 0
+        else:
+            if not (num & mask1 and not (num & mask2)):
+                return False
+        n_bytes -= 1
+    return n_bytes == 0
+
+
+if __name__ == "__main__":
+    data = [65]
+    print(validUTF8(data))
+
+    data = [80, 121, 116, 104, 111, 110, 32, 105,
+            115, 32, 99, 111, 111, 108, 33
+            ]
+    print(validUTF8(data))
+
+    data = [229, 65, 127, 256]
+    print(validUTF8(data))
