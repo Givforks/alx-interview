fcf80c464fcd149d1b7fb287ec006ad516c772ea
Givens Emmah Abraham <140185547+Givforks@users.noreply.github.com>
2024-07-26 12:09:56 +0100

    Update 0-validate_utf8.py

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,23 +7,24 @@ represents a valid UTF-8 encoding.
 
 def validUTF8(data):
     """Checks if a given dataset represents utf8."""
-    n_bytes, mask1, mask2 = 0, 1 << 7, 1 << 6
+    q, mask1, mask2 = 0, 1 << 7, 1 << 6
 
     for num in data:
         mask = 1 << 7
-        if n_bytes == 0:
+        if q == 0:
             while mask & num:
-                n_bytes += 1
+                q += 1
                 mask = mask >> 1
-            if n_bytes == 0:
+                """encoding iterations""" 
+            if q == 0:
                 continue
-            if n_bytes == 1 or n_bytes > 4:
+            if q == 1 or q > 4:
                 return False
         else:
             if not (num & mask1 and not (num & mask2)):
                 return False
-        n_bytes -= 1
-    return n_bytes == 0
+        q -= 1
+    return q == 0
 
 
 if __name__ == "__main__":
